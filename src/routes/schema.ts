2860e577a5dc2737ca703b7ef5119fc7c59a08a7
TeoDev1611 <malehurtadoreyes@hotmail.com>
2022-07-15 21:20:40 -0500

    feat: remove the draco and add own exists

--- src/routes/schema.ts
+++ src/routes/schema.ts
@@ -1,12 +1,23 @@
 import { Router } from 'opine/mod.ts';
 import { join } from 'path/mod.ts';
-import { dracoFiles } from 'draco/mod.ts';
 
 const router = Router();
 
+function exists(file: string): boolean {
+	try {
+		Deno.lstatSync(file);
+		return true;
+	} catch (err) {
+		if (err instanceof Deno.errors.NotFound) {
+			return false;
+		}
+		throw err;
+	}
+}
+
 router.get('/', async (_req, res, _next) => {
 	const path = join(Deno.cwd(), 'src', 'schema.json');
-	if (dracoFiles.exists(path) == false) {
+	if (exists(path) == false) {
 		res.setStatus(500).json({
 			error: 'Not found the file of the schema please report this on GitHub',
 		});
