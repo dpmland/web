42ab4956502e4e43b0af417bca25db0464ec5919
TeoDev1611 <malehurtadoreyes@hotmail.com>
2022-07-17 17:35:29 -0500

    feat: add the index.ejs to render

--- router/daddy.ts
+++ router/daddy.ts
@@ -3,6 +3,10 @@ import { Router } from 'oak/mod.ts';
 const R = new Router();
 
 R.get('/', (ctx) => {
+  ctx.render('index.ejs', { title: 'Deno Package Manager' });
+});
+
+R.get('/install', (ctx) => {
   ctx.response.body = 'Hello from installl';
 });
 
