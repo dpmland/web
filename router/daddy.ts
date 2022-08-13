af559b6145360063de15ab7b890a6808dff090e5
TeoDev1611 <malehurtadoreyes@hotmail.com>
2022-08-13 17:23:24 -0500

    feat: add the new urls and fix the url

--- router/daddy.ts
+++ router/daddy.ts
@@ -14,30 +14,10 @@ R.get('/install', (ctx: Context) => {
   );
 });
 
-R.get('/schema', async (ctx: Context) => {
-  function exists(file: string): boolean {
-    try {
-      Deno.lstatSync(file);
-      return true;
-    } catch (err) {
-      if (err instanceof Deno.errors.NotFound) {
-        return false;
-      }
-      throw err;
-    }
-  }
-  const path = join(Deno.cwd(), 'schema.json');
-  if (exists(path) == false) {
-    ctx.response.status = 500;
-    ctx.response.body = {
-      error: 'Not found the file of the schema please report this on GitHub',
-      code: 500,
-    };
-    return;
-  }
-  const file = await Deno.readTextFile(path);
-  ctx.response.type = 'application/json';
-  ctx.response.body = JSON.parse(file);
+R.get('/schema', (ctx: Context) => {
+  ctx.response.redirect(
+    'https://raw.githubusercontent.com/dpmland/dpm/main/schemas/dpm.json',
+  );
 });
 
 R.get('/discord', (ctx: Context) => {
