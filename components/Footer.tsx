db7d0a58a54846ae7b9add23d5b0fca52c1aa36d
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-09-01 12:51:43 -0500

    www: component footer.tsx created

---
+++ components/Footer.tsx
@@ -0,0 +1,31 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "../utils/twind.ts";
+
+const LINKS = [
+  {
+    title: "Source Code",
+    href: "https://github.com/dpmland/web/tree/dev",
+  },
+  {
+    title: "License",
+    href: "https://github.com/dpmland/web/blob/dev/LICENSE",
+  },
+];
+
+export default function Footer(): h.JSX.Element {
+  return (
+    <footer class={tw`flex justify-around bg-gray-100 mt-4`}>
+      <section class="">
+        {LINKS.map((link) => (
+          <a href={link.href} class={tw`m-3`}>
+            {link.title}
+          </a>
+        ))}
+      </section>
+      <section class="text(gray-100 center)">
+        <span>© {new Date().getFullYear()} dpm land</span>
+      </section>
+    </footer>
+  );
+}
