ccd836aa281b06d04f794f7c055d7d87c2766a4e
TeoDev1611 <malehurtadoreyes@hotmail.com>
2022-09-01 20:58:42 -0500

    feat: improve the format and lint command

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
-# fresh project
+# Dpm Web Fresh Version!
 
 ### Usage
 
-Start the project:
+- Start the project:
 
 ```
 deno task start
 ```
 
 This will watch the project directory and restart as necessary.
+
+### Tools
+
+- Format the project!
+
+```
+deno task fmt
+```
+
+- Check the format!
+
+```
+deno task check
+```
+
+---
+
+Made with :heart: in :earth_americas:
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,29 +1,29 @@
 /** @jsx h */
-import { h } from "preact";
-import { tw } from "../utils/twind.ts";
+import { h } from 'preact';
+import { tw } from '../utils/twind.ts';
 
 const LINKS = [
   {
-    title: "Source Code",
-    href: "https://github.com/dpmland/web/tree/dev",
+    title: 'Source Code',
+    href: 'https://github.com/dpmland/web/tree/dev',
   },
   {
-    title: "License",
-    href: "https://github.com/dpmland/web/blob/dev/LICENSE",
+    title: 'License',
+    href: 'https://github.com/dpmland/web/blob/dev/LICENSE',
   },
 ];
 
 export default function Footer(): h.JSX.Element {
   return (
     <footer class={tw`flex justify-around bg-gray-100 mt-4`}>
-      <section class="">
+      <section class=''>
         {LINKS.map((link) => (
           <a href={link.href} class={tw`m-3`}>
             {link.title}
           </a>
         ))}
       </section>
-      <section class="text(gray-100 center)">
+      <section class='text(gray-100 center)'>
         <span>© {new Date().getFullYear()} dpm land</span>
       </section>
     </footer>
