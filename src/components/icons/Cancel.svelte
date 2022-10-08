ad81013f01b669b2ec76124da6d4d41b19fd4d2d
Enzo <enzovivallo05@outlook.com>
2022-10-08 15:27:37 -0300

    feat: add homepage

---
+++ src/components/icons/Cancel.svelte
@@ -0,0 +1,7 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...$$props}>
+  <path
+    d="M12 24a12 12 0 1 1 12-12 12.013 12.013 0 0 1-12 12zm0-22a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2z"
+    fill="currentColor"
+  />
+  <path d="m3.292 19.293 16-16 1.414 1.415-16 16z" fill="currentColor" />
+</svg>
