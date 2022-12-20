db55e9fe9cff98b22e09784f5441ba129bee4a5b
Enzo <enzovivallo05@outlook.com>
2022-12-19 23:35:54 -0300

    refactor: formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
-import svelte from '@astrojs/svelte';
-
+import { defineConfig } from 'astro/config'
+import image from '@astrojs/image'
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), svelte()]
-});
+  integrations: [image(), svelte()],
+})
